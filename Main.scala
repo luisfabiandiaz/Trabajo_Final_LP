d7f1bd30d1a3130ac13a4d84b1a2f067d7362e31
unknown <luisfadial05@gmail.com>
2026-06-16 10:31:33 -0500

    Inicio de scala

---
+++ Main.scala
@@ -0,0 +1,8 @@
+//> using scala 3
+
+@main def inicio(): Unit =
+  println("¡Entorno configurado correctamente!")
+  
+  // En Scala 3 no necesitas llaves {} si usas la indentación correcta, igual que en Python
+  val mensaje = "Listo para el trabajo final de LP"
+  println(mensaje)
